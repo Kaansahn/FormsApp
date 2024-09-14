f98b9c21e5ee58429173ba6a6ff531fa34f38b98
Kaan <kaan.sahin2@hotmail.com>
2024-09-14 07:38:07 +0300

    Product, Category, and Repository models are created

---
+++ Models/Repository.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _products = new();
+        private static readonly List<Category> _categories = new();
+
+        static Repository()
+        {
+
+        }
+
+        public static List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+
+        public static List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+        }
+    }
+}
