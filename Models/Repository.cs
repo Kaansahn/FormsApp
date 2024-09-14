be1dd9c3ec47ce1cbc5e1896da9f7cb0fe9e2dd8
Kaan <kaan.sahin2@hotmail.com>
2024-09-14 08:46:02 +0300

    Product List

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -12,7 +12,14 @@ namespace FormsApp.Models
 
         static Repository()
         {
+            _categories.Add(new Category { CategoryId = 1, Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Bilgisayar" });
 
+            _products.Add(new Product { ProductId = 1, Name = "Iphone 14", Price = 40000, IsActive = true, Image = "1.jpg", CategoryId = 1 });
+            _products.Add(new Product { ProductId = 2, Name = "Iphone 15", Price = 45000, IsActive = true, Image = "2.jpeg", CategoryId = 1 });
+            _products.Add(new Product { ProductId = 3, Name = "Iphone 16", Price = 50000, IsActive = true, Image = "3.jpeg", CategoryId = 1 });
+            _products.Add(new Product { ProductId = 4, Name = "Macbook Air", Price = 60000, IsActive = true, Image = "4.jpeg", CategoryId = 2 });
+            _products.Add(new Product { ProductId = 5, Name = "Macbook Pro", Price = 70000, IsActive = true, Image = "5.jpg", CategoryId = 2 });
         }
 
         public static List<Product> Products
--- obj/Debug/net7.0/FormsApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/FormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd87650284ccc3548ad88ea67319d04a5f31bd25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98b9c21e5ee58429173ba6a6ff531fa34f38b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
